7d53c4fd8fa9ec88b4f69c6ee20f8bcef4f229ff
fukayatsu <fukayatsu@gmail.com>
2012-12-20 19:30:53 +0900

    check config file exists

--- testing-code-bot.rb
+++ testing-code-bot.rb
@@ -2,4 +2,15 @@
 
 require 'yaml'
 
-config = YAML.load_file('config.yml')
+Encoding.default_external = Encoding::UTF_8
+
+BASE_PATH   = File.expand_path(File.dirname(__FILE__))
+CONFIG_FILE = File.join(BASE_PATH, 'config.yml')
+
+unless File.exist?(CONFIG_FILE)
+  puts 'copy config.yml.sample to config.yml and set your keys'
+  exit 1
+end
+
+config = YAML.load_file(CONFIG_FILE)
+
